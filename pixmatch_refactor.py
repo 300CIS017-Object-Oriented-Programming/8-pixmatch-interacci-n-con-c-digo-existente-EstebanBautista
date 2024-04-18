9b5537cf10d8ab28596f79c9b2d1fd063ab25a74
EstebanBautista <124476884+EstebanBautista@users.noreply.github.com>
2024-04-18 13:20:22 -0500

    Update pixmatch_refactor.py

--- pixmatch_refactor.py
+++ pixmatch_refactor.py
@@ -55,13 +55,17 @@ if "emoji_bank" not in mystate: mystate.emoji_bank = []
 if "GameDetails" not in mystate: mystate.GameDetails = ['Medium', 6, 7, '']  # difficulty level, sec interval for autogen, total_cells_per_row_or_col, player name
 
 # common functions
+
+#Esta función reduce la separación entre el tope de la página y la sección especificada.
 def ReduceGapFromPageTop(wch_section = 'main page'):
     if wch_section == 'main page': st.markdown(" <style> div[class^='block-container'] { padding-top: 2rem; } </style> ", True) # main area
     elif wch_section == 'sidebar': st.markdown(" <style> div[class^='st-emotion-cache-10oheav'] { padding-top: 0rem; } </style> ", True) # sidebar
     elif wch_section == 'all': 
         st.markdown(" <style> div[class^='block-container'] { padding-top: 2rem; } </style> ", True) # main area
         st.markdown(" <style> div[class^='st-emotion-cache-10oheav'] { padding-top: 0rem; } </style> ", True) # sidebar
-    
+
+
+#Esta función maneja las acciones relacionadas con la tabla de clasificación del juego.
 def Leaderboard(what_to_do):
     if what_to_do == 'create':
         if mystate.GameDetails[3] != '':
@@ -101,16 +105,20 @@ def Leaderboard(what_to_do):
                         elif rknt == 2: sc2.write(f"🥈 | {leaderboard[vkey]['NameCountry']}: :red[{leaderboard[vkey]['HighestScore']}]")
                         elif rknt == 3: sc3.write(f"🥈 | {leaderboard[vkey]['NameCountry']}: :red[{leaderboard[vkey]['HighestScore']}]")
 
+
+#Esta función configura y muestra la página inicial del juego.
 def InitialPage():
     with st.sidebar:
+    # Configuración de la barra lateral con el título y el logotipo
         st.subheader("🖼️ Pix Match:")
         st.markdown(horizontal_bar, True)
-
+    # Carga y muestra el logotipo en la barra lateral
         # sidebarlogo = Image.open('sidebarlogo.jpg').resize((300, 420))
         sidebarlogo = Image.open('sidebarlogo.jpg').resize((300, 390))
         st.image(sidebarlogo, use_column_width='auto')
 
     # ViewHelp
+    # Configuración y visualización de la página principal del juego
     hlp_dtl = f"""<span style="font-size: 26px;">
     <ol>
     <li style="font-size:15px";>Game play opens with (a) a sidebar picture and (b) a N x N grid of picture buttons, where N=6:Easy, N=7:Medium, N=8:Hard.</li>
@@ -122,6 +130,7 @@ def InitialPage():
     <li style="font-size:15px";>At the end of the game, if you have a positive score, you will have <strong>won</strong>; otherwise, you will have <strong>lost</strong>.</li>
     </ol></span>""" 
 
+    # Configuración y visualización de las instrucciones del juego y la imagen de ayuda
     sc1, sc2 = st.columns(2)
     random.seed()
     GameHelpImg = vpth + random.choice(["MainImg1.jpg", "MainImg2.jpg", "MainImg3.jpg", "MainImg4.jpg"])
@@ -133,60 +142,91 @@ def InitialPage():
     sc1.markdown(hlp_dtl, unsafe_allow_html=True)
     st.markdown(horizontal_bar, True)
 
+    # Información del autor mostrada en la página inicial
     author_dtl = "<strong>Happy Playing: 😎 Shawn Pereira: shawnpereira1969@gmail.com</strong>"
     st.markdown(author_dtl, unsafe_allow_html=True)
 
+
+#Esta función lee un archivo de imagen y retorna su representación a base64.
 def ReadPictureFile(wch_fl):
     try:
+        # Construye la ruta completa del archivo de imagen
         pxfl = f"{vpth}{wch_fl}"
+        # Lee el archivo en modo binario, lo codifica en base64 y lo decodifica como una cadena
         return base64.b64encode(open(pxfl, 'rb').read()).decode()
+    except:
+        # En caso de error al leer el archivo, devuelve una cadena vacía
+        return ""
 
-    except: return ""
 
+#Esta función verifica si un botón de la celda ha sido presionado y realiza acciones en consecuencia.
 def PressedCheck(vcell):
+    # Verifica si el botón de la celda no ha sido presionado previamente
     if mystate.plyrbtns[vcell]['isPressed'] == False:
+        # Marca el botón de la celda como presionado y agrega la celda a las celdas expiradas
         mystate.plyrbtns[vcell]['isPressed'] = True
         mystate.expired_cells.append(vcell)
 
+        # Verifica si el emoji del botón coincide con el emoji de la barra lateral
         if mystate.plyrbtns[vcell]['eMoji'] == mystate.sidebar_emoji:
+            # Marca el botón como verdadero y aumenta la puntuación según la dificultad del juego
             mystate.plyrbtns[vcell]['isTrueFalse'] = True
             mystate.myscore += 5
 
-            if mystate.GameDetails[0] == 'Easy': mystate.myscore += 5
-            elif mystate.GameDetails[0] == 'Medium': mystate.myscore += 3
-            elif mystate.GameDetails[0] == 'Hard': mystate.myscore += 1
+            if mystate.GameDetails[0] == 'Easy':
+                mystate.myscore += 5
+            elif mystate.GameDetails[0] == 'Medium':
+                mystate.myscore += 3
+            elif mystate.GameDetails[0] == 'Hard':
+                mystate.myscore += 1
         
         else:
+            # Marca el botón como falso y disminuye la puntuación en 1
             mystate.plyrbtns[vcell]['isTrueFalse'] = False
             mystate.myscore -= 1
 
+
+#Esta función restablece el tablero del juego al inicio de una nueva partida.
 def ResetBoard():
+    # Determina la cantidad total de celdas por fila o columna según la configuración del juego
     total_cells_per_row_or_col = mystate.GameDetails[2]
 
-    sidebar_emoji_no = random.randint(1, len(mystate.emoji_bank))-1
+    # Selecciona aleatoriamente un emoji de la barra lateral y lo asigna como emoji de la barra lateral
+    sidebar_emoji_no = random.randint(1, len(mystate.emoji_bank)) - 1
     mystate.sidebar_emoji = mystate.emoji_bank[sidebar_emoji_no]
 
+    # Marca si el emoji de la barra lateral está presente en la lista de emojis de los botones del tablero
     sidebar_emoji_in_list = False
-    for vcell in range(1, ((total_cells_per_row_or_col ** 2)+1)):
-        rndm_no = random.randint(1, len(mystate.emoji_bank))-1
+    for vcell in range(1, ((total_cells_per_row_or_col ** 2) + 1)):
+        rndm_no = random.randint(1, len(mystate.emoji_bank)) - 1
         if mystate.plyrbtns[vcell]['isPressed'] == False:
             vemoji = mystate.emoji_bank[rndm_no]
             mystate.plyrbtns[vcell]['eMoji'] = vemoji
-            if vemoji == mystate.sidebar_emoji: sidebar_emoji_in_list = True
+            if vemoji == mystate.sidebar_emoji:
+                sidebar_emoji_in_list = True
 
-    if sidebar_emoji_in_list == False:  # sidebar pix is not on any button; add pix randomly
-        tlst = [x for x in range(1, ((total_cells_per_row_or_col ** 2)+1))]
+    # Si el emoji de la barra lateral no está en la lista de emojis de los botones
+    if sidebar_emoji_in_list == False:
+        tlst = [x for x in range(1, ((total_cells_per_row_or_col ** 2) + 1))]
         flst = [x for x in tlst if x not in mystate.expired_cells]
         if len(flst) > 0:
-            lptr = random.randint(0, (len(flst)-1))
+            lptr = random.randint(0, (len(flst) - 1))
             lptr = flst[lptr]
             mystate.plyrbtns[lptr]['eMoji'] = mystate.sidebar_emoji
 
+
+#Esta función prepara el inicio de un nuevo juego, configurando variables y bancos de emojis según la dificultad seleccionada.
 def PreNewGame():
+
+    ## Establece la cantidad total de celdas por fila o columna según la configuración del juego
     total_cells_per_row_or_col = mystate.GameDetails[2]
+
+    # Reinicia la lista de celdas expiradas y el puntaje del jugador
     mystate.expired_cells = []
     mystate.myscore = 0
 
+
+    # Define diferentes bancos de emojis según la dificultad del juego
     foxes = ['😺', '😸', '😹', '😻', '😼', '😽', '🙀', '😿', '😾']
     emojis = ['😃', '😄', '😁', '😆', '😅', '😂', '🤣', '😊', '😇', '🙂', '🙃', '😉', '😌', '😍', '🥰', '😘', '😗', '😙', '😚', '😋', '😛', '😝', '😜', '🤪', '🤨', '🧐', '🤓', '😎', '🤩', '🥳', '😏', '😒', '😞', '😔', '😟', '😕', '🙁', '☹️', '😣', '😖', '😫', '😩', '🥺', '😢', '😠', '😳', '😥', '😓', '🤗', '🤔', '🤭', '🤫', '🤥', '😶', '😐', '😑', '😬', '🙄', '😯', '😧', '😮', '😲', '🥱', '😴', '🤤', '😪', '😵', '🤐', '🥴', '🤒']
     humans = ['👶', '👧', '🧒', '👦', '👩', '🧑', '👨', '👩‍🦱', '👨‍🦱', '👩‍🦰', '‍👨', '👱', '👩', '👱', '👩‍', '👨‍🦳', '👩‍🦲', '👵', '🧓', '👴', '👲', '👳'] 
@@ -201,6 +241,7 @@ def PreNewGame():
     blue_signs = ['🚾', '♿️', '🅿️', '🈂️', '🛂', '🛃', '🛄', '🛅', '🚹', '🚺', '🚻', '🚮', '🎦', '📶', '🈁', '🔣', '🔤', '🔡', '🔠', '🆖', '🆗', '🆙', '🆒', '🆕', '🆓', '0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟', '🔢', '⏏️', '▶️', '⏸', '⏯', '⏹', '⏺', '⏭', '⏮', '⏩', '⏪', '⏫', '⏬', '◀️', '🔼', '🔽', '➡️', '⬅️', '⬆️', '⬇️', '↗️', '↘️', '↙️', '↖️', '↪️', '↩️', '⤴️', '⤵️', '🔀', '🔁', '🔂', '🔄', '🔃', '➿', '🔚', '🔙', '🔛', '🔝', '🔜']
     moon = ['🌕', '🌔', '🌓', '🌗', '🌒', '🌖', '🌑', '🌜', '🌛', '🌙']
 
+    # Selecciona aleatoriamente un banco de emojis según la dificultad del juego y lo asigna como emoji_bank
     random.seed()
     if mystate.GameDetails[0] == 'Easy':
         wch_bank = random.choice(['foods', 'moon', 'animals'])
@@ -214,50 +255,75 @@ def PreNewGame():
         wch_bank = random.choice(['foxes', 'emojis', 'humans', 'foods', 'clocks', 'hands', 'animals', 'vehicles', 'houses', 'purple_signs', 'red_signs', 'blue_signs', 'moon'])
         mystate.emoji_bank = locals()[wch_bank]
 
+    # Inicializa el diccionario de botones del jugador con sus estados iniciales
     mystate.plyrbtns = {}
     for vcell in range(1, ((total_cells_per_row_or_col ** 2)+1)): mystate.plyrbtns[vcell] = {'isPressed': False, 'isTrueFalse': False, 'eMoji': ''}
 
+
+#Esta función devuelve un emoji basado en el puntaje del jugador, indicando su estado emocional según el puntaje.
 def ScoreEmoji():
+    #puntaje es cero
     if mystate.myscore == 0: return '😐'
+    #puntaje está entre -5 y -1
     elif -5 <= mystate.myscore <= -1: return '😏'
+    #puntaje está entre -10 y -6
     elif -10 <= mystate.myscore <= -6: return '☹️'
+    #puntaje es menor o igual a -11
     elif mystate.myscore <= -11: return '😖'
+    #puntaje está entre 1 y 5
     elif 1 <= mystate.myscore <= 5: return '🙂'
+    #puntaje está entre 6 y 10
     elif 6 <= mystate.myscore <= 10: return '😊'
+    #puntaje es mayor que 10
     elif mystate.myscore > 10: return '😁'
 
+
+#Esta función inicia un nuevo juego, restableciendo el tablero y mostrando la interfaz de usuario correspondiente.
 def NewGame():
+
+    ## Reinicia el tablero y obtiene la cantidad total de celdas por fila o columna
     ResetBoard()
     total_cells_per_row_or_col = mystate.GameDetails[2]
 
+    # Reducir el espacio desde la parte superior de la página
     ReduceGapFromPageTop('sidebar')
     with st.sidebar:
+
+        # Crea la interfaz de usuario en la barra lateral para el nuevo juego
         st.subheader(f"🖼️ Pix Match: {mystate.GameDetails[0]}")
         st.markdown(horizontal_bar, True)
 
         st.markdown(sbe.replace('|fill_variable|', mystate.sidebar_emoji), True)
 
+        # Configura el temporizador de actualización automática y actualiza el puntaje del jugador
         aftimer = st_autorefresh(interval=(mystate.GameDetails[1] * 1000), key="aftmr")
         if aftimer > 0: mystate.myscore -= 1
 
         st.info(f"{ScoreEmoji()} Score: {mystate.myscore} | Pending: {(total_cells_per_row_or_col ** 2)-len(mystate.expired_cells)}")
 
         st.markdown(horizontal_bar, True)
+
+        # Botón para regresar a la página principal
         if st.button(f"🔙 Return to Main Page", use_container_width=True):
             mystate.runpage = Main
             st.rerun()
-    
+
+
+    # Actualiza el tablero y muestra la posición de las imágenes
     Leaderboard('read')
     st.subheader("Picture Positions:")
     st.markdown(horizontal_bar, True)
 
     # Set Board Dafaults
+    # Establece las configuraciones predeterminadas del tablero
     st.markdown("<style> div[class^='css-1vbkxwb'] > p { font-size: 1.5rem; } </style> ", unsafe_allow_html=True)  # make button face big
 
+    # Crea columnas para las celdas del tablero
     for i in range(1, (total_cells_per_row_or_col+1)):
         tlst = ([1] * total_cells_per_row_or_col) + [2] # 2 = rt side padding
         globals()['cols' + str(i)] = st.columns(tlst)
     
+    # Recorre las celdas del tablero y muestra las imágenes correspondientes
     for vcell in range(1, (total_cells_per_row_or_col ** 2)+1):
         if 1 <= vcell <= (total_cells_per_row_or_col * 1):
             arr_ref = '1'
@@ -314,9 +380,11 @@ def NewGame():
     st.caption('') # vertical filler
     st.markdown(horizontal_bar, True)
 
+    # Verifica si todas las celdas han sido presionadas
     if len(mystate.expired_cells) == (total_cells_per_row_or_col ** 2):
         Leaderboard('write')
 
+        # Muestra efectos visuales según el puntaje
         if mystate.myscore > 0: st.balloons()
         elif mystate.myscore <= 0: st.snow()
 
@@ -324,17 +392,24 @@ def NewGame():
         mystate.runpage = Main
         st.rerun()
 
+#Esta función representa la página principal del juego, donde se elige el nivel de dificultad y se inicia un nuevo juego.
 def Main():
+
+    # Reducir el ancho de la barra lateral y establecer el color de los botones
     st.markdown('<style>[data-testid="stSidebar"] > div:first-child {width: 310px;}</style>', unsafe_allow_html=True,)  # reduce sidebar width
     st.markdown(purple_btn_colour, unsafe_allow_html=True)
 
+    # Muestra la página inicial del juego
     InitialPage()
     with st.sidebar:
+        # Configura las opciones de nivel de dificultad y nombre del jugador en la barra lateral
         mystate.GameDetails[0] = st.radio('Difficulty Level:', options=('Easy', 'Medium', 'Hard'), index=1, horizontal=True, )
         mystate.GameDetails[3] = st.text_input("Player Name, Country", placeholder='Shawn Pereira, India', help='Optional input only for Leaderboard')
 
+        # Botón para iniciar un nuevo juego
         if st.button(f"🕹️ New Game", use_container_width=True):
 
+            # Configura las variables del juego según el nivel de dificultad seleccionado
             if mystate.GameDetails[0] == 'Easy':
                 mystate.GameDetails[1] = 8         # secs interval
                 mystate.GameDetails[2] = 6         # total_cells_per_row_or_col
@@ -347,6 +422,7 @@ def Main():
                 mystate.GameDetails[1] = 5         # secs interval
                 mystate.GameDetails[2] = 8         # total_cells_per_row_or_col
 
+            # Crea la tabla de clasificación y prepara el nuevo juego
             Leaderboard('create')
 
             PreNewGame()
